86e19d0f03ff5842a2c043de1497cace17ff245b
Sérgio Simões <sergiohero@gmail.com>
2016-03-15 10:44:44 +0000

    Testes para bookie terminados

--- tests/bookie_tests.rb
+++ tests/bookie_tests.rb
@@ -3,7 +3,6 @@ require_relative "../modules/APICollection"
 include APICollection
 
 
-
 class BookieTests
 
   @bookies={}
@@ -15,6 +14,20 @@ class BookieTests
   def addBookie(name, password)
     bookie = Bookie.new(name, password)
     return APICollection.put(@bookies, bookie.name, bookie)
+  end
+
+  def remBookie(name)
+    return APICollection.rem(@bookies, name)
+  end
+
+  def updateBookie(key,name, password)
+    bookie=APICollection.get(@bookies, key)
+    if (bookie)
+      bookie.name=name
+      bookie.password=password
+      return true
+    end
+    return false
   end
 
 end
--- tests/tests.rb
+++ tests/tests.rb
@@ -4,10 +4,13 @@ require "test/unit"
 
 class Tests < Test::Unit::TestCase
 
-  def test_insertBookie
+  def test_Bookies
     bookieTest = BookieTests.new
     assert_equal(true, bookieTest.addBookie("Sérgio Simões", "123"), "Inserir um Bookie 1")
     assert_equal(true, bookieTest.addBookie("David Rodriges", "1234"), "Inserir um Bookie 2")
     assert_equal(false, bookieTest.addBookie("Sérgio Simões", "123134"), "Inserir Bookie com username existente")
+
+    assert_equal(false,bookieTest.remBookie("Zé da Esquina"),"Remover bookie inexistente")
+    assert_equal(true,bookieTest.updateBookie("Sérgio Simões","Rodolfo Reis","123"))
   end
 end
